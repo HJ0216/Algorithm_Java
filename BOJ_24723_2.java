97ae3452ccfe6b7cc9bf34687b51212fd02f7358
HJ0216 <sso01215@naver.com>
2023-09-07 11:46:30 +0900

    Docs: add BOJ_24723.java

---
+++ BOJ_24723_2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int sum = 1;
+		for (int i = 0; i < N; i++) {
+			sum *= 2;
+		}
+
+		bw.write(sum + "");
+
+		bw.flush();
+		bw.close();
+	}
+}
